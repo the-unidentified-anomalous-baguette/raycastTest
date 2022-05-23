af5e5194e9543a5caf7266ac1bb3151c259bf5a8
the-unidentified-anomalous-baguette <enabull2002@gmail.com>
2022-05-23 17:19:01 +0100

    basic game file
    an html and js files with p5 structure

---
+++ script.js
@@ -0,0 +1,10 @@
+function setup(){
+    rectMode(CENTER)
+    ellipseMode(CENTER)
+    canvas = createCanvas(1024, 576)
+    canvas.parent('container')
+}
+
+function draw(){
+    background(100)
+}
