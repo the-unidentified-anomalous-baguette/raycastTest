d2abd0554d6342cd93fb4e8798d1c39d3a8c5e48
the-unidentified-anomalous-baguette <enabull2002@gmail.com>
2022-05-24 21:07:17 +0100

    Player move real smooth
    Working player movement with top-down rendering, but first person style controls

--- script.js
+++ script.js
@@ -1,10 +1,97 @@
+let map = [
+  [1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1],
+  [1, 0, 0, 0, 1, 1],
+  [1, 1, 1, 1, 1, 1]]
+
+let pX = 80
+let pY = 80
+let pRadius = 10
+let pSpeed = 20
+let mapSize = 64
+let pDir = 0
+
 function setup(){
-    rectMode(CENTER)
-    ellipseMode(CENTER)
-    canvas = createCanvas(1024, 576)
-    canvas.parent('container')
+  rectMode(CENTER)
+  ellipseMode(CENTER)
+  angleMode(DEGREES)
+  canvas = createCanvas(1024, 576)
+  canvas.parent('container')
 }
 
 function draw(){
-    background(100)
+  background(100)
+  for (let i = 0; i < map.length; i++){
+    for (let j = 0; j < map[i].length; j++){
+      if (map[i][j] == 1){
+        rect(j * mapSize + mapSize/2, i * mapSize + mapSize/2, mapSize, mapSize)
+      }
+    }
+  }
+  if (keyIsDown(37)){
+    pDir -= 3
+  }
+  if (keyIsDown(39)){
+    pDir += 3
+  }
+  if (keyIsDown(87)){
+    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) + pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) + pRadius) / mapSize)] != 1){
+      pX += pSpeed*sin(pDir)
+    }
+    if (map[Math.floor((pY - pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY - pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY - pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY - pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
+      pY -= pSpeed*cos(pDir)
+    }
+  }
+  if (keyIsDown(83)){
+    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) + pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) + pRadius) / mapSize)] != 1){
+      pX -= pSpeed*sin(pDir)
+    }
+    if (map[Math.floor((pY + pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY + pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY + pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY + pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
+      pY += pSpeed*cos(pDir)
+    }
+  }
+  if (keyIsDown(65)){
+    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) + pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) + pRadius) / mapSize)] != 1){
+      pX -= pSpeed*cos(pDir)
+    }
+    if (map[Math.floor((pY - pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY - pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY - pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY - pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
+      pY -= pSpeed*sin(pDir)
+    }
+  }
+  if (keyIsDown(68)){
+    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) + pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) + pRadius) / mapSize)] != 1){
+      pX += pSpeed*cos(pDir)
+    }
+    if (map[Math.floor((pY + pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
+        map[Math.floor((pY + pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY + pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
+        map[Math.floor((pY + pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
+      pY += pSpeed*sin(pDir)
+    }
+  }
+  rect(pX, pY, pRadius * 2, pRadius * 2)
+  line(pX, pY, pX + 40 *sin(pDir), pY - 40 * cos(pDir))
 }
