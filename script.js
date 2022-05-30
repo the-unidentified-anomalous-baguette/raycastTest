338e3f8f6bb5d1110031c9ba28577541184a55c0
the-unidentified-anomalous-baguette <76777929+the-unidentified-anomalous-baguette@users.noreply.github.com>
2022-05-30 19:36:50 +0100

    Rays casting and colliding
    I got a working ray that can be moved and knows when it's colliding with a 'boundary'

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-let map = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+class boundary{
+  constructor(x1, y1, x2, y2){
+    this.a = createVector(x1, y1)
+    this.b = createVector(x2, y2)
+  }
 
-let pX = 80
-let pY = 80
-let pRadius = 15
-let pSpeed = 5
-let mapSize = 16
-let pDir = 0
+  show(){
+    line(this.a.x, this.a.y, this.b.x, this.b.y)
+  }
+}
+
+class ray{
+  constructor(x, y){
+    this.pos = createVector(x, y)
+    this.dir = createVector(1, -1)
+  }
+
+  show(u){
+    this.dir.x = sin(pAngle) * u
+    this.dir.y = -cos(pAngle) * u
+    push()
+    translate(this.pos.x, this.pos.y)
+    line(0, 0, this.dir.x * 1000, this.dir.y * 1000)
+    pop()
+  }
+
+  cast(wall){
+    let x1 = wall.a.x
+    let x2 = wall.b.x
+    let y1 = wall.a.y
+    let y2 = wall.b.y
+
+    circle(wall.a.x, wall.a.y, 20)
+    circle(wall.b.x, wall.b.y, 20)
+
+    let x3 = this.pos.x
+    let x4 = x3 + this.dir.x
+    let y3 = this.pos.y
+    let y4 = y3 + this.dir.y
+
+    let pt = createVector()
+
+    let den = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)
+    if (den == 0){return}
+    let t = ((x1-x3)*(y3-y4)-(y1-y3)*(x3-x4))/den
+    let u = ((x1-x3)*(y1-y2)-(y1-y3)*(x1-x2))/den
+    this.show(u)
+    //console.log(u)
+    if (t >= 0 && t <= 1 && u >= 0){
+      pt.x = ((x1*y2 - y1*x2)*(x3 - x4) - (x1-x2)*(x3*y4 - y3*x4))/den
+      pt.y = ((x1*y2 - y1*x2)*(y3 - y4) - (y1-y2)*(x3*y4 - y3*x4))/den
+      circle(pt.x, pt.y, 20)
+      console.log(u, sqrt((x3 - pt.x)*(x3 - pt.x) + (y3 - pt.y)*(y3 - pt.y)))
+      //console.log(true)
+      return pt
+    } else {return}
+  }
+}
+
+let bound1
+let bound2
+let ray1
+let pAngle = 0
 
 function setup(){
   rectMode(CENTER)
@@ -23,83 +68,24 @@ function setup(){
   angleMode(DEGREES)
   canvas = createCanvas(1024, 576)
   canvas.parent('container')
+  stroke(255)
+  fill(25)
+  bound1 = new boundary(200, 100, 300, 500)
+  bound2 = new boundary(700, 50, 1000, 570)
+  ray1 = new ray(500, 400)
 }
 
 function draw(){
   background(100)
-  noStroke()
-  fill((255, 255, 255))
-  for (let i = 0; i < map.length; i++){
-    for (let j = 0; j < map[i].length; j++){
-      if (map[i][j] == 1){
-        rect(j * mapSize + mapSize/2, i * mapSize + mapSize/2, mapSize, mapSize)
-      }
-    }
-  }
-  stroke(0)
+  bound1.show()
+  bound2.show()
+  ray1.show(1)
   if (keyIsDown(37)){
-    pDir -= 3
+    pAngle -= 1
   }
   if (keyIsDown(39)){
-    pDir += 3
+    pAngle += 1
   }
-  if (keyIsDown(87)){
-    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) + pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*sin(pDir) + pRadius) / mapSize)] != 1){
-      pX += pSpeed*sin(pDir)
-    }
-    if (map[Math.floor((pY - pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY - pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY - pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY - pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
-      pY -= pSpeed*cos(pDir)
-    }
-  }
-  if (keyIsDown(83)){
-    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) - pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) + pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*sin(pDir) + pRadius) / mapSize)] != 1){
-      pX -= pSpeed*sin(pDir)
-    }
-    if (map[Math.floor((pY + pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY + pSpeed*cos(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY + pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY + pSpeed*cos(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
-      pY += pSpeed*cos(pDir)
-    }
-  }
-  if (keyIsDown(65)){
-    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) + pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX - pSpeed*cos(pDir) + pRadius) / mapSize)] != 1){
-      pX -= pSpeed*cos(pDir)
-    }
-    if (map[Math.floor((pY - pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY - pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY - pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY - pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
-      pY -= pSpeed*sin(pDir)
-    }
-  }
-  if (keyIsDown(68)){
-    if (map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) - pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY-pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) + pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY+pRadius) / mapSize)][Math.floor((pX + pSpeed*cos(pDir) + pRadius) / mapSize)] != 1){
-      pX += pSpeed*cos(pDir)
-    }
-    if (map[Math.floor((pY + pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 && 
-        map[Math.floor((pY + pSpeed*sin(pDir) - pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY + pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX-pRadius) / mapSize)] != 1 &&
-        map[Math.floor((pY + pSpeed*sin(pDir) + pRadius) / mapSize)][Math.floor((pX+pRadius) / mapSize)] != 1){
-      pY += pSpeed*sin(pDir)
-    }
-  }
-  fill((120, 50, 50))
-  rect(pX, pY, pRadius * 2, pRadius * 2)
-  line(pX, pY, pX + 40 *sin(pDir), pY - 40 * cos(pDir))
+  ray1.cast(bound1)
+  ray1.cast(bound2)
 }
