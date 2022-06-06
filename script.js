f929c08aebaa5f0e792f55adc76ab62d378665de
the-unidentified-anomalous-baguette <76777929+the-unidentified-anomalous-baguette@users.noreply.github.com>
2022-06-06 09:10:04 +0100

    rendering tweaks
    changed rendering to better account for variation in wall height, used shearing to let players look up and down

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+let red = [200,10, 10]
+let stone = [127, 127, 127]
+
 class boundary{
-  constructor(x1, y1, x2, y2){
+  constructor(x1, y1, x2, y2, colour, height, base){
     this.a = createVector(x1, y1)
     this.b = createVector(x2, y2)
     this.hyp = dist(x1, y1, x2, y2)
     this.adj = dist(x1, y1, x2, y1)
     this.cosAng = acos(this.adj/this.hyp)
+    this.colour = colour
+    this.height = height
+    this.base = base
   }
 
   show(){
@@ -75,11 +81,14 @@ class ray{
 }
 
 class pc{
-  constructor(x, y, angle, speed){
+  constructor(x, y, z, angle, speed, height){
     this.x = x
     this.y = y
     this.angle = angle
     this.speed = speed
+    this.z = z
+    this.height = height
+    this.eyeLevel = z + height
   }
 
   parallelMove(wall){
@@ -98,42 +107,87 @@ let rayReturn
 let renderDist = 288
 let horizon = 288
 
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
+    }
+}
+
+function renderCalc(){
+  seenWalls = []
+  for (let i = player.angle - 45; i <= player.angle + 45; i++){
+    for (let j = 0; j < walls.length; j++){
+      rayReturn = new ray(i).cast(walls[j])
+      if (rayReturn <= 1){
+        seenWalls.push([rayReturn, i, walls[j]])
+        // for (let k = 0; k < seenWalls.length - 1; k++){
+        //   if (seenWalls[k][1] == seenWalls[seenWalls.length - 1][1]){
+        //     if (seenWalls[k][0] < seenWalls[seenWalls.length - 1][0]){
+        //       seenWalls.pop()
+        //     }
+        //     else {
+        //       seenWalls.splice(k, 1)
+        //     }
+        //   }
+        // }
+      }
+    }
+  }
+  seenWalls.sort(sortFunction);
+}
+
 function setup(){
   rectMode(CORNER)
   ellipseMode(CENTER)
   angleMode(DEGREES)
+  colorMode(RGB, 255)
   canvas = createCanvas(1024, 576)
   canvas.parent('container')
   stroke(255)
   fill(25)
-  walls = [new boundary(100, 100, 200, 500), new boundary(200, 500, 300, 310), new boundary(500, 300, 700, 100), 
-           new boundary(700, 100, 100, 100), new boundary(300, 300, 310, 300), new boundary(310, 300, 310, 310),
-           new boundary(300, 310, 300, 300), new boundary(310, 310, 500, 300), new boundary(300, 300, 230, 230),
-           new boundary(200, 200, 220, 200), new boundary(220, 200, 230, 230), new boundary(230, 230, 190, 205), 
-           new boundary(190, 205, 200, 200)]
+  walls = [new boundary(3, 5, 57, 8, stone, 50), new boundary(57, 8, 67, 29, stone, 50), new boundary(67, 29, 101, 34, red, 50), 
+           new boundary(101, 34, 116, 22, stone, 50), new boundary(116, 22, 145, 24, stone, 50), new boundary(145, 24, 181, 59, stone, 50),
+           new boundary(151, 50, 140, 71, stone, 50), new boundary(112, 70, 100, 39, stone, 50), new boundary(151, 50, 164, 77, stone, 50),
+           new boundary(100, 39, 66, 39, red, 50), new boundary(66, 39, 49, 69, stone, 50), new boundary(49, 69, 3, 57, stone, 50), 
+           new boundary(3, 57, 3, 5, stone, 50), new boundary(140, 71, 140, 83, stone, 50), new boundary(140, 83, 158, 101, stone, 50), 
+           new boundary(158, 101, 164, 93, stone, 50), new boundary(164, 93, 156, 85, stone, 50), new boundary(156, 85, 164, 77, stone, 50), 
+           new boundary(195, 95, 195, 123, stone, 50), new boundary(195, 123, 156, 133, stone, 50), new boundary(181, 59, 195, 95, red, 50),
+           new boundary(156, 133, 149, 107, stone, 50), new boundary(149, 107, 112, 70, stone, 50)]
   frameRate(30)
-  player = new pc(150, 150, 45, 2)
+  player = new pc(10, 10, 0, 45, 2, 288)
+  renderCalc()
 }
 
 function draw(){
   background(color(100, 100, 255))
   fill(100, 50, 0)
-  rect(0, horizon, 1024, 576 - horizon)
-  for (let i = 288; i < 576; i++){
-    fill(0, 0, 0, 255 - 255 * (i*i)/(577*577))
-    rect(0, i, 1024, 1)
-  }
-  seenWalls = []
+  
   if (keyIsDown(37)){
     player.angle -= player.speed
+    renderCalc()
   }
   if (keyIsDown(39)){
     player.angle += player.speed
+    renderCalc()
   }
-  if (keyIsDown(16)){
-    horizon = 188
+  if (keyIsDown(40)){
+    horizon -= player.speed
   }
-  else {horizon = 288}
+  if (keyIsDown(38)){
+    horizon += player.speed
+  }
+  // if (keyIsDown(16)){
+  //   horizon = 188
+  // }
+  //else {horizon = 288}
+  rect(0, horizon, 1024, 576 - horizon)
+  for (let i = 0; i < 576 - horizon; i++){
+    fill(0, 0, 0, 255 - (255 * i/576))
+    rect(0, i + horizon, 1024, 1)
+  }
   if (keyIsDown(87)){
     let canFw = false
     for (let i = 0; i < walls.length; i++){
@@ -145,6 +199,7 @@ function draw(){
     if (canFw){
       player.x += sin(player.angle)
       player.y += -cos(player.angle)
+      renderCalc()
     }
   }
   if (keyIsDown(83)){
@@ -158,6 +213,7 @@ function draw(){
     if (canBw){
       player.x -= sin(player.angle)
       player.y -= -cos(player.angle)
+      renderCalc()
     }
   }
   if (keyIsDown(65)){
@@ -171,6 +227,7 @@ function draw(){
     if (canLw){
       player.x += -cos(player.angle)
       player.y -= sin(player.angle)
+      renderCalc()
     }
   }
   if (keyIsDown(68)){
@@ -184,27 +241,20 @@ function draw(){
     if (canRw){
       player.x -= -cos(player.angle)
       player.y += sin(player.angle)
-    }
-  }
-  for (let i = player.angle - 45; i <= player.angle + 45; i++){
-    for (let j = 0; j < walls.length; j++){
-      rayReturn = new ray(i).cast(walls[j])
-      if (rayReturn <= 1){
-        seenWalls.push([rayReturn, i])
-        for (let k = 0; k < seenWalls.length - 1; k++){
-          if (seenWalls[k][1] == seenWalls[seenWalls.length - 1][1]){
-            if (seenWalls[k][0] < seenWalls[seenWalls.length - 1][0]){
-              seenWalls.pop()
-            }
-          }
-        }
-      }
+      renderCalc()
     }
   }
   noStroke()
   for (let i = 0; i < seenWalls.length; i++){
-    fill(2048 / (255 * seenWalls[i][0] * seenWalls[i][0]), 512 / (255 * seenWalls[i][0] * seenWalls[i][0]), 0)
-    rect(1024 * (45+seenWalls[i][1] - player.angle)/90, horizon - 144 * (1-Math.pow(seenWalls[i][0], -0.5)), 12, 288 * (1-Math.pow(seenWalls[i][0], -0.5)))
+    fill(seenWalls[i][2].colour[0], seenWalls[i][2].colour[1], seenWalls[i][2].colour[2])
+    rect(1024 * (45+seenWalls[i][1] - player.angle)/90, horizon - seenWalls[i][2].height * (1-Math.pow(seenWalls[i][0], -0.9))/2 , 
+    12, seenWalls[i][2].height * (1-Math.pow(seenWalls[i][0], -0.5)))
+    fill(0, 0, 0, 256 * seenWalls[i][0] * seenWalls[i][0] * 8)
+    rect(1024 * (45+seenWalls[i][1] - player.angle)/90, horizon - seenWalls[i][2].height * (1-Math.pow(seenWalls[i][0], -0.9))/2 , 
+    12, seenWalls[i][2].height * (1-Math.pow(seenWalls[i][0], -0.5)))
+    // fill(2048 / (255 * seenWalls[i][0] * seenWalls[i][0]), 0, 0, (256 / (255 * seenWalls[i][0] * seenWalls[i][0]))/256 - 50)
+    // rect(1024 * (45+seenWalls[i][1] - player.angle)/90, horizon - 144 * (1-Math.pow(seenWalls[i][0], -0.5)), 
+    // 12, 288 * (1-Math.pow(seenWalls[i][0], -0.5)))
   }strokeWeight(1)
   for (let i = 0; i < walls.length; i++){
     stroke((0, 255, 0))
@@ -213,4 +263,5 @@ function draw(){
   noStroke()
   fill(0)
   circle(player.x, player.y, 10)
+  text(Math.floor(frameRate()), 10, 20);
 }
